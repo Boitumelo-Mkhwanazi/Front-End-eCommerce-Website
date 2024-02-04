6ba95475b452233040638ba3b946bf6e4185b909
Boitumelo <mkwanazibuti082@gmail.com>
2024-02-04 23:14:22 +0200

    Declaring Arrays

--- scripts/main-last-days.js
+++ scripts/main-last-days.js
@@ -1,1 +1,248 @@
-console.log('hello buti');
+//data for the home page
+const homeProducts = [{
+    image: 'pictures/Catalogue/Cat normal043.jpg',
+    singleProductImages:  {
+        image1: 'pictures/pictures/home products/Cat normal01.jpg',
+        image2: 'pictures/home products/Cat normal01_1.jpg',
+        image3: 'pictures/home products/Cat normal01_2.jpg',
+        image4: 'pictures/home products/Cat normal01_3.jpg'
+    },
+    name: 'Tea t-shirt',
+    priceCents: 50000
+}, {
+    image: 'pictures/Catalogue/Cat normal008.jpg',
+    singleProductImages: {
+        image1: 'pictures/home products/Cat normal02.jpg',
+        image2: 'pictures/home products/Cat normal02_1.jpg',
+        image3: 'pictures/home products/Cat normal02_2.jpg',
+        image4: 'pictures/home products/Cat normal02_3.jpg'
+    },
+    name: 'Military Jacket',
+    priceCents: 100000
+}, {
+    image: 'pictures/Catalogue/Cat normal018.jpg',
+    singleProductImages: {
+        image1: 'pictures/home products/Cat normal03.jpg',
+        image2: 'pictures/home products/Cat normal03_1.jpg',
+        image3: 'pictures/home products/Cat normal03_2.jpg',
+        image4: 'pictures/home products/Cat normal03_3.jpg'
+    },
+    name: 'Bird graphic Puncho shirt',
+    priceCents: 55000
+}, {
+    image: 'pictures/Catalogue/Cat normal022.jpg',
+    singleProductImages: {
+        image1: 'pictures/home products/Cat normal04.jpg',
+        image2: 'pictures/home products/Cat normal04_1.jpg',
+        image3: 'pictures/home products/Cat normal04_2.jpg',
+        image4: 'pictures/home products/Cat normal04_3.jpg'
+    },
+    name: 'Shoulder-cut shirt',
+    priceCents: 55000
+}, {
+    image: 'pictures/Catalogue/Cat normal005.jpg',
+    singleProductImages: {
+        image1: 'pictures/home products/Cat normal05.jpg',
+        image2: 'pictures/home products/Cat normal05_1.jpg',
+        image3: 'pictures/home products/Cat normal05_2.jpg',
+        image4: 'pictures/home products/Cat normal05_3.jpg'
+    },
+    name: 'High Waist Pants',
+    priceCents: 65000
+}, {
+    image: 'pictures/Catalogue/Cat normal037.jpg',
+    singleProductImages: {
+        image1: 'pictures/home products/Cat normal06.jpg',
+        image2: 'pictures/home products/Cat normal06_1.jpg',
+        image3: 'pictures/home products/Cat normal06_2.jpg',
+        image4: 'pictures/home products/Cat normal06_3.jpg'
+    },
+    name: 'Khakhi Dashed Trousers',
+    priceCents: 70000
+}, {
+    image: 'pictures/Catalogue/Cat normal047.jpg',
+    singleProductImages: {
+        image1: 'pictures/home products/Cat normal07.jpg',
+        image2: 'pictures/home products/Cat normal07_1.jpg',
+        image3: 'pictures/home products/Cat normal07_2.jpg',
+        image4: 'pictures/home products/Cat normal07_3.jpg'
+    },
+    name: 'Bermuda Shorts',
+    priceCents: 40000
+}, {
+    image: 'pictures/Catalogue/Cat normal041.jpg',
+    singleProductImages: {
+        image1: 'pictures/home products/Cat normal08.jpg',
+        image2: 'pictures/home products/Cat normal08_1.jpg',
+        image3: 'pictures/home products/Cat normal08_2.jpg',
+        image4: 'pictures/home products/Cat normal08_3.jpg'
+    },
+    name: 'Luggage-tag Bag',
+    priceCents: 75000
+}];
+
+//data for the shop page
+const shopProducts = [{
+    image: 'pictures/Catalogue/Cat normal029.jpg',
+    singleProductImages: {
+        image1: 'pictures/shop products/Cat normal1.jpg',
+        image2: 'pictures/shop products/Cat normal1_1.jpg',
+        image3: 'pictures/shop products/Cat normal1_2.jpg',
+        image4: 'pictures/shop products/Cat normal1_3.jpg'
+    },
+    name: 'Sleeveless Knit Golfer',
+    priceCents: 85000
+}, {
+    image: 'pictures/Catalogue/Cat normal016.jpg',
+    singleProductImages: {
+        image1: 'pictures/shop products/Cat normal2.jpg',
+        image2: 'pictures/shop products/Cat normal2_1.jpg',
+        image3: 'pictures/shop products/Cat normal2_2.jpg',
+        image4: 'pictures/shop products/Cat normal2_3.jpg'
+    },
+    name: 'Logo Reversible Bucket Hat',
+    priceCents: 30000
+}, {
+    image: 'pictures/Catalogue/Cat normal017.jpg',
+    singleProductImages: {
+        image1: 'pictures/shop products/Cat normal3.jpg',
+        image2: 'pictures/shop products/Cat normal3_1.jpg',
+        image3: 'pictures/shop products/Cat normal3_2.jpg',
+        image4: 'pictures/shop products/Cat normal3_3.jpg'
+    },
+    name: 'Logo Reversible Bucket Hat',
+    priceCents: 30000
+}, {
+    image: 'pictures/Catalogue/Cat normal043.jpg',
+    singleProductImages: {
+        image1: 'pictures/shop products/Cat normal4.jpg',
+        image2: 'pictures/shop products/Cat normal4_1.jpg',
+        image3: 'pictures/shop products/Cat normal4_2.jpg',
+        image4: 'pictures/shop products/Cat normal4_3.jpg'
+    },
+    name: 'Tea t-shirt',
+    priceCents: 50000
+}, {
+    image: 'pictures/Catalogue/Cat normal008.jpg',
+    singleProductImages: {
+        image1: 'pictures/shop products/Cat normal5.jpg',
+        image2: 'pictures/shop products/Cat normal5_1.jpg',
+        image3: 'pictures/shop products/Cat normal5_2.jpg',
+        image4: 'pictures/shop products/Cat normal5_3.jpg'
+    },
+    name: 'Military Jacket',
+    priceCents: 100000
+}, {
+    image: 'pictures/Catalogue/Cat normal018.jpg',
+    singleProductImages: {
+        image1: 'pictures/shop products/Cat normal6.jpg',
+        image2: 'pictures/shop products/Cat normal6_1.jpg',
+        image3: 'pictures/shop products/Cat normal6_2.jpg',
+        image4: 'pictures/shop products/Cat normal6_3.jpg'
+    },
+    name: 'Bird graphic Puncho shirt',
+    priceCents: 90000
+}, {
+    image: 'pictures/Catalogue/Cat normal022.jpg',
+    singleProductImages: {
+        image1: 'pictures/shop products/Cat normal7.jpg',
+        image2: 'pictures/shop products/Cat normal7_1.jpg',
+        image3: 'pictures/shop products/Cat normal7_2.jpg',
+        image4: 'pictures/shop products/Cat normal7_3.jpg'
+    },
+    name: 'Shoulder-cut Shirt',
+    priceCents: 55000
+}, {
+    image: 'pictures/Catalogue/Cat normal005.jpg',
+    singleProductImages: {
+        image1: 'pictures/shop products/Cat normal8.jpg',
+        image2: 'pictures/shop products/Cat normal8_1.jpg',
+        image3: 'pictures/shop products/Cat normal8_2.jpg',
+        image4: 'pictures/shop products/Cat normal8_3.jpg'
+    },
+    name: 'High Waist Pants',
+    priceCents: 65000
+}, {
+    image: 'pictures/Catalogue/Cat normal037.jpg',
+    singleProductImages: {
+        image1: 'pictures/shop products/Cat normal9.jpg',
+        image2: 'pictures/shop products/Cat normal9_1.jpg',
+        image3: 'pictures/shop products/Cat normal9_2.jpg',
+        image4: 'pictures/shop products/Cat normal9_3.jpg'
+    },
+    name: 'Khakhi Dashed Trousers',
+    priceCents: 70000
+}, {
+    image: 'pictures/Catalogue/Cat normal047.jpg',
+    singleProductImages: {
+        image1: 'pictures/shop products/Cat normal10.jpg',
+        image2: 'pictures/shop products/Cat normal10_1.jpg',
+        image3: 'pictures/shop products/Cat normal10_2.jpg',
+        image4: 'pictures/shop products/Cat normal10_3.jpg'
+    },
+    name: 'Bermuda Shorts',
+    priceCents: 40000
+}, {
+    image: 'pictures/Catalogue/Cat normal041.jpg',
+    singleProductImages: {
+        image1: 'pictures/shop products/Cat normal11.jpg',
+        image2: 'pictures/shop products/Cat normal11_1.jpg',
+        image3: 'pictures/shop products/Cat normal11_2.jpg',
+        image4: 'pictures/shop products/Cat normal11_3.jpg'
+    },
+    name: 'Luggage-tag Bag',
+    priceCents: 75000
+}, {
+    image: 'pictures/Catalogue/Cat normal001.jpg',
+    singleProductImages: {
+        image1: 'pictures/shop products/Cat normal12.jpg',
+        image2: 'pictures/shop products/Cat normal12_1.jpg',
+        image3: 'pictures/shop products/Cat normal12_2.jpg',
+        image4: 'pictures/shop products/Cat normal12_3.jpg'
+    },
+    name: 'Luggage-tag Bag',
+    priceCents: 40000
+}];
+
+//data for single product page 
+const featuredProducts = [{
+    image: 'pictures/Catalogue/Cat normal043.jpg',
+    singleProductImages: {
+        image1: 'pictures/product featured products/Cat normal01.jpg',
+        image2: 'pictures/product featured products/Cat normal01_1.jpg',
+        image3: 'pictures/product featured products/Cat normal01_2.jpg',
+        image4: 'pictures/product featured products/Cat normal01_3.jpg'
+    },
+    name: 'Tea t-shirt',
+    priceCents: 50000
+}, {
+    image: 'pictures/Catalogue/Cat normal018.jpg',
+    singleProductImages: {
+        image1: 'pictures/product featured products/Cat normal03.jpg',
+        image2: 'pictures/product featured products/Cat normal03_1.jpg',
+        image3: 'pictures/product featured products/Cat normal03_2.jpg',
+        image4: 'pictures/product featured products/Cat normal03_3.jpg'
+    },
+    name: 'Bird graphic Puncho shirt',
+    priceCents: 90000
+}, {
+    image: 'pictures/Catalogue/Cat normal016.jpg',
+    singleProductImages: {
+        image1: 'pictures/product featured products/Cat normal016.jpg',
+        image2: 'pictures/product featured products/Cat normal016.jpg',
+        image3: 'pictures/product featured products/Cat normal016.jpg',
+        image4: 'pictures/product featured products/Cat normal016.jpg'
+    },
+    name: 'Logo Reversible Bucket Hat',
+    priceCents: 30000
+}, {
+    image: 'pictures/Catalogue/Cat normal008.jpg',
+    singleProductImages: {
+        image1: 'pictures/product featured products/Cat normal02.jpg',
+        image2: 'pictures/product featured products/Cat normal02_1.jpg',
+        image3: 'pictures/product featured products/Cat normal02_2.jpg',
+        image4: 'pictures/product featured products/Cat normal0_3.jpg'
+    },
+    name: 'Military Jacket',
+    priceCents: 100000
+}];
