91458121ad326fe2deba5d98c8c132f944d86b13
Boitumelo <mkwanazibuti082@gmail.com>
2024-02-04 23:43:35 +0200

    Generate Home Content HTML with JavaScript

--- scripts/main-last-days.js
+++ scripts/main-last-days.js
@@ -28,7 +28,7 @@ const homeProducts = [{
         image4: 'pictures/home products/Cat normal03_3.jpg'
     },
     name: 'Bird graphic Puncho shirt',
-    priceCents: 55000
+    priceCents: 90000
 }, {
     image: 'pictures/Catalogue/Cat normal022.jpg',
     singleProductImages: {
@@ -246,3 +246,25 @@ const featuredProducts = [{
     name: 'Military Jacket',
     priceCents: 100000
 }];
+
+let homeProductsHTML = '';
+
+homeProducts.forEach((homeProduct) => {
+    homeProductsHTML += `
+        <div class="product-box">
+            <img src="${homeProduct.image}" alt="" class="product-img">
+            <h2 class="product-title">R ${homeProduct.name}</h2>
+            <span class="price">R ${(homeProduct.priceCents / 100).toFixed(2)}</span>
+            <div class="cart-add-container">
+                <div class="cart-add">
+                    <i class="fa-solid fa-bag-shopping add-cart"></i>
+                </div>
+                <div class="add-cart-text">
+                    Add to cart
+                </div>
+            </div>
+        </div>
+    `;
+});
+
+document.querySelector('.js-home-shop-content').innerHTML = homeProductsHTML;
