f6d6aec71b0b2eda85112771dc20555db86dc90c
Boitumelo <mkwanazibuti082@gmail.com>
2024-02-08 08:00:19 +0200

    adding double quotations in our arrays

--- scripts/featured-products-last-days.js
+++ scripts/featured-products-last-days.js
@@ -1,46 +1,46 @@
 const featuredProducts = [{
-    id: 'a1',
-    image: 'pictures/Catalogue/Cat normal043.jpg',
+    id: "a1",
+    image: "pictures/Catalogue/Cat normal043.jpg",
     singleProductImages: {
-        image1: 'pictures/product featured products/Cat normal01.jpg',
-        image2: 'pictures/product featured products/Cat normal01_1.jpg',
-        image3: 'pictures/product featured products/Cat normal01_2.jpg',
-        image4: 'pictures/product featured products/Cat normal01_3.jpg'
+        image1: "pictures/product featured products/Cat normal01.jpg",
+        image2: "pictures/product featured products/Cat normal01_1.jpg",
+        image3: "pictures/product featured products/Cat normal01_2.jpg",
+        image4: "pictures/product featured products/Cat normal01_3.jpg"
     },
-    name: 'Tea t-shirt',
+    name: "Tea t-shirt",
     priceCents: 50000
 }, {
-    id: 'a3',
-    image: 'pictures/Catalogue/Cat normal018.jpg',
+    id: "a3",
+    image: "pictures/Catalogue/Cat normal018.jpg",
     singleProductImages: {
-        image1: 'pictures/product featured products/Cat normal03.jpg',
-        image2: 'pictures/product featured products/Cat normal03_1.jpg',
-        image3: 'pictures/product featured products/Cat normal03_2.jpg',
-        image4: 'pictures/product featured products/Cat normal03_3.jpg'
+        image1: "pictures/product featured products/Cat normal03.jpg",
+        image2: "pictures/product featured products/Cat normal03_1.jpg",
+        image3: "pictures/product featured products/Cat normal03_2.jpg",
+        image4: "pictures/product featured products/Cat normal03_3.jpg"
     },
-    name: 'Bird graphic Puncho shirt',
+    name: "Bird graphic Puncho shirt",
     priceCents: 90000
 }, {
-    id: 'a10',
-    image: 'pictures/Catalogue/Cat normal016.jpg',
+    id: "a10",
+    image: "pictures/Catalogue/Cat normal016.jpg",
     singleProductImages: {
-        image1: 'pictures/product featured products/Cat normal016.jpg',
-        image2: 'pictures/product featured products/Cat normal016.jpg',
-        image3: 'pictures/product featured products/Cat normal016.jpg',
-        image4: 'pictures/product featured products/Cat normal016.jpg'
+        image1: "pictures/product featured products/Cat normal016.jpg",
+        image2: "pictures/product featured products/Cat normal016.jpg",
+        image3: "pictures/product featured products/Cat normal016.jpg",
+        image4: "pictures/product featured products/Cat normal016.jpg"
     },
-    name: 'Logo Reversible Bucket Hat',
+    name: "Logo Reversible Bucket Hat",
     priceCents: 30000
 }, {
     id: 'a2',
-    image: 'pictures/Catalogue/Cat normal008.jpg',
+    image: "pictures/Catalogue/Cat normal008.jpg",
     singleProductImages: {
-        image1: 'pictures/product featured products/Cat normal02.jpg',
-        image2: 'pictures/product featured products/Cat normal02_1.jpg',
-        image3: 'pictures/product featured products/Cat normal02_2.jpg',
-        image4: 'pictures/product featured products/Cat normal0_3.jpg'
+        image1: "pictures/product featured products/Cat normal02.jpg",
+        image2: "pictures/product featured products/Cat normal02_1.jpg",
+        image3: "pictures/product featured products/Cat normal02_2.jpg",
+        image4: "pictures/product featured products/Cat normal0_3.jpg"
     },
-    name: 'Military Jacket',
+    name: "Military Jacket",
     priceCents: 100000
 }];
 
